4db49e7e731762b20e140972d83d77184efc37ab
Zexylz <69458582+Zexylz@users.noreply.github.com>
2024-08-10 18:32:51 -0700

    Update get-note.js

--- get-note.js
+++ get-note.js
@@ -1,6 +1,24 @@
+const NetlifyAPI = require('netlify')
+
 exports.handler = async function(event, context) {
+  const client = new NetlifyAPI(process.env.NETLIFY_AUTH_TOKEN)
+  const { note } = JSON.parse(event.body)
+
+  try {
+    await client.updateSiteBuildSetting({
+      site_id: process.env.SITE_ID,
+      setting_key: 'LOVE_NOTE',
+      value: note
+    })
     return {
       statusCode: 200,
-      body: JSON.stringify({ note: process.env.LOVE_NOTE || "Your love note will appear here" })
-    };
-  };
+      body: JSON.stringify({ success: true })
+    }
+  } catch (error) {
+    console.error('Error updating environment variable:', error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false, error: 'Failed to update environment variable' })
+    }
+  }
+}
